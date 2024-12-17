afbe3ecbcb202b0bbca21342dba5f49614ac7f58
Anas Alam <74352709+AnasAlam786@users.noreply.github.com>
2024-12-17 17:16:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request, session, url_for, re
 import os
 from dotenv import load_dotenv
 from werkzeug.security import check_password_hash
-from model import db, TeachersLogin, StudentData, updateScore, updateFees, StudentsDB, FeesDB
+from model import db, TeachersLogin, StudentData, updateScore, updateFees, StudentsDB, FeesDB ,updateParentsAdhar
 from bs4 import BeautifulSoup
 import datetime
 
@@ -49,6 +49,9 @@ def login():
     return render_template('login.html', error=error)
 
 
+@app.route('/studentModal', methods=["GET", "POST"])
+def studentModal():
+    return render_template('studentModal.html')
 
 
 @app.route('/getfees', methods=["GET", "POST"])
@@ -78,7 +81,7 @@ def getfees():
             monthName =  datetime.datetime.now().strftime("%B")
             monthIndex=list(data[0]["Fees"].keys()).index(monthName)+1
 
-            html = render_template('feesmodal.html',data=data,currentMonth=monthIndex)
+            html = render_template('feesModal.html',data=data,currentMonth=monthIndex)
             soup=BeautifulSoup(html,"lxml")
             content=soup.body.decode_contents()
             
@@ -256,69 +259,52 @@ def marks():
     else:
         return redirect(url_for('login'))
 
+@app.route('/aapar', methods=["GET", "POST"])
+def aapar():
+
+    if "email" in session:
+        data = None
+
+        if request.method == "POST":
+            payload = request.json
+
+            if payload.get('task')=='pageUpdate':
+
+                CLASS = payload.get('class')
+
+                data = StudentData("id","STUDENTS_NAME","ROLL", "FATHERS_NAME","MOTHERS_NAME","Parents_Aadhar",class_filter_json = {"CLASS": [CLASS]})
+    
+                html = render_template('aapar.html', data=data)
+                soup=BeautifulSoup(html,"lxml")
+                content=soup.body.find('div',{'id':'dataTable'}).decode_contents()
+
+                return jsonify({"html":str(content)})
+            
+            elif payload.get('task')=='aadhar':
+                id=payload.get('id')
+                mother=payload.get('Mother_Aadhar')
+                father=payload.get('Father_Aadhar')
+                data={}
+
+                
+                if (father=='' or len(father)==12):
+                    data["Father"]=father
+                else:
+                    return {"STATUS": 'FAILED'}
+                    
+                if (mother=='' or len(mother)==12):
+                    data["Mother"]=mother
+                else:
+                    return {"STATUS": 'FAILED'}
+                
+                
+                result = updateParentsAdhar(id, data)
+                return{"STATUS": result}
+            
+        return render_template('aapar.html', data=data)
+
+    else:
+        return redirect(url_for('login'))
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-"""
-GOOGLE_SHEETS_URL = "https://sheets.googleapis.com/v4/spreadsheets/1yGyqIyDWtaVK1z2LbvvtEDl1YpeIgWMwuAyUcIdr3Cc/values/Sheet1?key=AIzaSyCunanUcxEoloBYJR1EqhkD16-uWAxlQzY"
-
-@app.route('/getData/<CLASS>/<SUBJECT>',methods=['GET','POST'])
-def getData(CLASS, SUBJECT):
-    response = requests.get(GOOGLE_SHEETS_URL)
-
-    if response.status_code == 200:
-        jdata = response.json().get('values')
-        df = pd.DataFrame(jdata[1:], columns=jdata[0])
-        exam=f"FA1_{SUBJECT}"
-
-        filtered_df = df[df['CLASS'] == CLASS]
-        data = filtered_df[['CLASS', 'ROLL', exam]].to_dict(orient='records')
-        return jsonify(data)
-    else:
-        return "Failed"
-    return f"Class is {CLASS}"
-
-
-
-function SelectFunc() {
-          const CLASS = document.getElementById("Class").value;
-          const SUBJECT = document.getElementById("Subject").value;
-
-          if (SUBJECT !== "Subject" && CLASS !== "Class") {
-
-            fetch(`/getData/${CLASS}/${SUBJECT}`,{method: 'GET',
-                 headers: {
-                     'Content-Type': 'application/json'}
-                 })
-            .then(response => response.json())
-            .then(data => {
-              creatingRows(data, SUBJECT)
-              onEnter()
-            })
-            console.log("Hii there")
-          }
-
-
-        }  
-
-
-
-
-function onEnter(rows) {
-        focusedInput = document.activeElement
-
-        if (focusedInput && focusedInput.tagName === 'INPUT') {
-
-        focusedInput.addEventListener("keydown", (event) => {
-
-            if (event.key === "Enter") {
-                event.preventDefault()
-                button=focusedInput.nextElementSibling
-                submit(button)
-
-
-            }
-        });
-    }
-}"""
