9121582d28f0bbffed5da33ef877c62be2a207ee
Anas Alam <anasalam702@gmail.com>
2025-08-13 01:36:20 +0530

    Added pandas to req.txt

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = create_app()
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=5000, debug=True)
     app.run(debug=True)
- 
+    # To activate your virtual environment, run the following command in your terminal (not in this Python file):
+    # On Windows:
+    #   .\venv\Scripts\activate
+    # On macOS/Linux:
+    #   source venv/bin/activate
