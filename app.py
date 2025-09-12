109b4e3a5139f641ff8825103b34d8836040bd8a
Anas Alam <anasalam702@gmail.com>
2025-09-12 21:35:31 +0530

    Added the dynamic school name in question paper maker!

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # app.run(port=5000, debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
 
     #app.run(debug=True)
     # To activate your virtual environment, run the following command in your terminal (not in this Python file):
