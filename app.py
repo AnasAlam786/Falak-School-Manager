59d41458f117f7f39a7efb587bcc67079e19eee9
Anas Alam <anasalam702@gmail.com>
2025-09-20 07:19:53 +0530

    Setup to deploy on render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from src import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     # app.run(port=2000, debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # fallback for local testing
+    app.run(host="0.0.0.0", port=port, debug=True)
 
     #app.run(debug=True)
     # To activate your virtual environment, run the following command in your terminal (not in this Python file):
